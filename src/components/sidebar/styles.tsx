c89bab1fc4dfda8fa25a70d5800024b0c72746b0
Rafael Neves <rafa.mneves@hotmail.com>
2024-01-20 22:05:25 -0300

    style: add sidebar styles

---
+++ src/components/sidebar/styles.tsx
@@ -0,0 +1,53 @@
+import styled, { css } from 'styled-components'
+
+export const Wrapper = styled.aside`
+  width: 216px;
+  padding: 1rem;
+
+  ${({ theme }) => css`
+    background-color: ${theme.colors.primary['900']};
+  `}
+`
+
+type SidebarItemProps = {
+  isActive: boolean
+}
+
+export const SidebarItem = styled.a<SidebarItemProps>`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  position: relative;
+  padding: 0.5rem;
+  transition: all 0.2s;
+  text-decoration: none;
+
+  :hover {
+    opacity: 0.8;
+  }
+
+  ${({ theme, isActive }) => css`
+    color: ${isActive
+      ? theme.colors.neutral['300']
+      : theme.colors.neutral['900']};
+
+    > span {
+      font-size: 1.2rem;
+      font-weight: 600;
+      font-family: ${theme.fontFamily.figtree};
+    }
+
+    &:before {
+      content: '';
+      position: absolute;
+      left: -0.2rem;
+      top: 0.2rem;
+      width: 4px;
+      height: 27px;
+      display: ${isActive ? 'block' : 'none'};
+      background-color: ${isActive
+        ? theme.colors.primary['900']
+        : theme.colors.white};
+    }
+  `}
+`
